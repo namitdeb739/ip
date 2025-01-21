7cae5a983abd3e3cc001b6a56107b47bc07e3211
namitdeb739 <namitdeb739@gmail.com>
2025-01-22 00:47:35 +0800

    Make Cypher echo user input until 'bye' is input

--- src/main/java/Cypher.java
+++ src/main/java/Cypher.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Cypher {
     public static void main(String[] args) {
         String logo = " ██████╗██╗   ██╗██████╗ ██╗  ██╗███████╗██████╗\n"
@@ -7,10 +9,31 @@ public class Cypher {
                 + "╚██████╗   ██║   ██║     ██║  ██║███████╗██║  ██║\n"
                 + "╚═════╝   ╚═╝   ╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝\n";
 
-        System.out.println("__________________________________________");
+        System.out.println("____________________");
         System.out.println("Hello, I'm \n" + logo);
         System.out.println("What can I do for you?");
-        System.out.println("__________________________________________");
+        System.out.println("____________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        do {
+            input = scanner.nextLine();
+            switch (input) {
+            case "bye":
+                break;
+            default:
+                System.out.println("____________________");
+                System.out.println(input);
+                System.out.println("____________________");
+                break;
+            }
+        } while (!input.equals("bye"));
+
+        System.out.println("____________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________");
+
+        scanner.close();
     }
 }
