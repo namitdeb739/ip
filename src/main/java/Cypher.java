dd38afcad3ffac2d691a1e1e72386fe699104b49
namitdeb739 <namitdeb739@gmail.com>
2025-01-22 00:35:45 +0800

    Name chatbot 'Cypher', add greeting and farewell text

---
+++ src/main/java/Cypher.java
@@ -0,0 +1,16 @@
+public class Cypher {
+    public static void main(String[] args) {
+        String logo = " ██████╗██╗   ██╗██████╗ ██╗  ██╗███████╗██████╗\n"
+                + "██╔════╝╚██╗ ██╔╝██╔══██╗██║  ██║██╔════╝██╔══██╗\n"
+                + "██║      ╚████╔╝ ██████╔╝███████║█████╗  ██████╔╝\n"
+                + "██║       ╚██╔╝  ██╔═══╝ ██╔══██║██╔══╝  ██╔══██╗\n"
+                + "╚██████╗   ██║   ██║     ██║  ██║███████╗██║  ██║\n"
+                + "╚═════╝   ╚═╝   ╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝\n";
+
+        System.out.println("__________________________________________");
+        System.out.println("Hello, I'm \n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("__________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
